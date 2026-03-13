949d7b3dce617b100e985dacade62a0c59ebcbc6
Stefano Cozzula <stefano.cozzula.stud@ispascalcomandini.it>
2026-03-13 09:29:27 +0100

    Aggiunto commento e modificato come avviene l'estrazione

--- EstrazioneLettere/EstrazioneLettere/MainWindow.xaml.cs
+++ EstrazioneLettere/EstrazioneLettere/MainWindow.xaml.cs
@@ -10,9 +10,7 @@ namespace EstrazioneLettere
     {
         char[] _alfabeto = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
 
-        List<char> _listLettereParola = new List<char>(); //Lista che contiene le lettere estratte per formare la parola
-
-        string _parola; //Parola che si forma con le lettere estratte
+        string _parola = ""; //Parola che si forma con le lettere estratte
 
         int index = 0; //Indice per estrarre lettere dall'alfabeto
 
@@ -23,6 +21,7 @@ namespace EstrazioneLettere
         int _numeroCaratteri = 5; //Numero di caratteri da estrarre per parola
 
         bool _iniziataEstrazione = false; //Per capire se l'estrazione è iniziata
+
         public MainWindow()
         {
             InitializeComponent();
@@ -43,24 +42,18 @@ namespace EstrazioneLettere
         {
             if (_iniziataEstrazione == true)
             {
-                _listLettereParola.Add(_lettera); //Aggiungo la lettera estratta alla lista parola
-                _parola = ""; //inizializzo la stringa parola a vuoto
-
-                foreach (char c in _listLettereParola) //per ogni carattere c nella lista parola, aggiungo alla stringa parola il carattere c
-                {
-                    _parola += c; //Aggiorno la parola con ogni carattere estratto
-                }
-
-                if (_parola.Length == _numeroCaratteri) //Capisco se la parola ha raggiunto il numero di caratteri da estrarre desiderato
-                {
-                    lstParole.Items.Add(_parola); //Aggiungo la parola alla listbox di parole
-
-                    _listLettereParola.Clear(); //Pulisco la lista per la parola per estrarre una nuova parola
-                    _parola = ""; //Pulisco la stringa parola per estrarre una nuova parola
-                }
+                _parola += _lettera; //Aggiorno la parola con ogni carattere estratto
 
                 txtParola.Clear();
-                txtParola.Text = _parola.ToString();
+
+                txtParola.Text = _parola.ToString(); //Aggiorno il textbox con la parola formata dalle lettere estratte
+
+                if (_parola.Length >= _numeroCaratteri) //Capisco se la parola ha raggiunto il numero di caratteri da estrarre desiderato
+                {
+                    lstParole.Items.Add(_parola); //Aggiungo la parola alla listbox di parole
+                    _parola = ""; //inizializzo la parola vuota
+                }
+
                 await Task.Delay(100);
             }
         }
@@ -83,7 +76,6 @@ namespace EstrazioneLettere
                 _numeroCaratteri--; //Diminuisco il numero di caratteri da estrarre
                 txtNumeroCaratteri.Clear();
             }
-
         }
     }
 }
